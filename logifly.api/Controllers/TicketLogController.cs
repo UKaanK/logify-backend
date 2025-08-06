887c732d2366935941b8bd61df558394e35db493
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2025-08-07 02:39:28 +0300

    TicketLogController.cs Endpointlere Açıklama Eklendi

--- logifly.api/Controllers/TicketLogController.cs
+++ logifly.api/Controllers/TicketLogController.cs
@@ -2,6 +2,7 @@
 using logifly.application.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace logifly.api.Controllers
 {
@@ -16,6 +17,11 @@ namespace logifly.api.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [SwaggerOperation(Summary = "Sistemdeki Ticketler Getirilir.")]
+        [SwaggerResponse(200, "Başarıyla Ticketler Getirildi")]
+        [SwaggerResponse(400, "Geçersiz istek && Ticketler Bulunamadı")]
         public async Task<IActionResult> GetAllAsync()
         {
             var logs = await _ticketLogService.GetAllAsync();
@@ -23,12 +29,22 @@ namespace logifly.api.Controllers
         }
 
         [HttpGet("{ticketId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [SwaggerOperation(Summary = "Yeni bir destek talebi oluşturur.")]
+        [SwaggerResponse(200, "Başarıyla oluşturuldu")]
+        [SwaggerResponse(400, "Geçersiz istek && Ticket Bulunamadı")]
         public async Task<IActionResult> GetByTicketIdAsync(Guid ticketId)
         {
             var logs = await _ticketLogService.GetByTicketIdAsync(ticketId);
             return Ok(logs);
         }
         [HttpPost]
+        [ProducesResponseType(typeof(TicketLogCreateDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [SwaggerOperation(Summary = "Yeni bir destek Log talebi oluşturur.")]
+        [SwaggerResponse(201, "Başarıyla oluşturuldu", typeof(TicketLogCreateDto))]
+        [SwaggerResponse(400, "Geçersiz istek")]
         public async Task<IActionResult> AddAsync([FromBody] TicketLogCreateDto request)
         {
            await _ticketLogService.AddAsync(request);
